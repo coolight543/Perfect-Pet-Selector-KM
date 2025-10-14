0286ce1d8abf72181bd3658a4dc60bf01fdc2b22
coolight543 <robamiao@gmail.com>
2025-10-14 15:04:52 +0000

    km 10 14 25

--- PetSelector.java
+++ PetSelector.java
@@ -4,6 +4,8 @@ public class PetSelector{
     public static void main(String [] args){
         //get user input
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
         System.out.print("Choose your favorite color from the options: red, blue, or green ");
         String color = scanner.nextLine();
         if (color.indexOf("red") == 0 || color.indexOf("blue") == 0 || color.indexOf("green") == 0)
@@ -15,14 +17,41 @@ public class PetSelector{
             {
                 if (color.indexOf("blue")==0 && season.indexOf("fall")  == 0)
                 {
-                    System.out.print("Alligator");
+                    System.out.print("Alligator \n");
                 }
                 else if(color.indexOf("blue") == 0 && season.indexOf("spring") == 0)
                 {
-                    System.out.print("ostrich");
+                    System.out.print("ostrich \n");
+                }
+                else if(color.indexOf("green") == 0 && season.indexOf("winter") == 0 && name.indexOf("[AEIOUaeiou]")!=0)
+                {
+                    System.out.print("giraffe \n");
+                }
+                else if(color.indexOf("green") == 0 && season.indexOf("fall") != 0 && season.indexOf("winter")!=0)
+                {
+                    System.out.print("dog\n");
+                }
+                else if(color.indexOf("red") == 0)
+                {
+                    if(name.indexOf("[AEIOUaeiou]")==0)
+                    {
+                        System.out.print("porcupine\n");
+                    }
+                    else
+                    {
+                    System.out.print("panda\n");
+                    }
+                }
+                else if (season.indexOf("summer")==0 && color.indexOf("green") !=0 && color.indexOf("red") !=0)
+                {
+                    System.out.print("ponies\n");
+                }
+                else if (name.indexOf("[AEIOUaeiou]") !=0 && color.indexOf("blue") ==0 && season.indexOf("winter") == 0)
+                {
+                    System.out.print("axolotl");
                 }
                 else{
-                    System.out.print("rock");
+                    System.out.print("rock\n");
                 }
             }
         else
